b6218a44fa9b90a773d6b1c77b98617f9d579481
adamnapieralski <adam.napieralski@gmail.com>
2022-01-03 07:19:07 +0000

    feat(ga-evaluation): add handling difficult range only, capitalize consts

--- genetic_algorithms/evaluate.py
+++ genetic_algorithms/evaluate.py
@@ -8,10 +8,13 @@ from shepherd_simulation import Decision_type
 from shepherd_simulation import ShepherdSimulation
 from datetime import datetime
 
-no_timesteps = 8000
-max_no_neighbours = 139
-no_sims_per_combination = 50
-verbose = True
+NO_TIMESTEPS = 8000
+MAX_NO_NEIGHBOURS = 139
+NO_SIMS_PER_COMBINATION = 50
+DIFFICULT_RANGE_ONLY = True
+VERBOSE = True
+
+RESULTS_INITIAL_FILL_VAL = -1.
 
 evaluated_counter = 0
 total_evaluations_num = 0
@@ -29,37 +32,44 @@ def _sim_with_agents(N, n, id, total_evaluations_num, start_time):
     Helper function needed for parallelization.
     Computes the simulation result for one specifc N, n.
     """
-    if verbose:
+    if VERBOSE:
         print(f"Evaluating N: {N}\tn: {n}\tprocess_id: {mp.current_process().name[len('ForkPoolWorker-'):]}\telapsed: {str(datetime.now() - start_time).split('.')[0]}\tprogress: {round(100 * id / total_evaluations_num, 2)}")
 
     result = []
     # Repeat simulation for 50 times, 8000 time steps each (default parameter)
     avg_success = 0
-    for _ in range(no_sims_per_combination):
-        sim = ShepherdSimulation(num_sheep_total=N, num_sheep_neighbors=n, max_steps=no_timesteps, decision_type=DECISION_TYPE, random_state=np.random.RandomState())
+    for _ in range(NO_SIMS_PER_COMBINATION):
+        sim = ShepherdSimulation(num_sheep_total=N, num_sheep_neighbors=n, max_steps=NO_TIMESTEPS, decision_type=DECISION_TYPE, random_state=np.random.RandomState())
         sim.set_thresh_field_params(DECISION_PARAMS)
         _, success, _ = sim.run()
         avg_success += success
-    avg_success /= no_sims_per_combination
+    avg_success /= NO_SIMS_PER_COMBINATION
 
     res = None
-    with open(result_file, 'rb') as f:
+    with open(RESULT_FILE, 'rb') as f:
         res = np.load(f)
         res[N, n] = avg_success
     if res is not None:
-        with open(result_file, 'wb') as f:
+        with open(RESULT_FILE, 'wb') as f:
             np.save(f, res)
     return { (N, n): result }
 
 def generate_N_n_pairs():
     pairs = []
-    for N in range(2, max_no_neighbours + 1):
+    for N in range(2, MAX_NO_NEIGHBOURS + 1):
         for n in range(1, N):
             pairs.append([N, n])
     return pairs
 
-def generate_simulations_args():
-    pairs_N_n = generate_N_n_pairs()
+def generate_N_n_pairs_difficult_range():
+    pairs = []
+    for N in range(30, MAX_NO_NEIGHBOURS + 1):
+        for n in range(int(np.floor(3 * np.log2(N))), int(np.ceil(0.53 * N))):
+            pairs.append([N, n])
+    return pairs
+
+def generate_simulations_args(difficult_range_only=False):
+    pairs_N_n = generate_N_n_pairs_difficult_range() if difficult_range_only else generate_N_n_pairs()
     total_evaluations_num = len(pairs_N_n)
     start_time = datetime.now()
     return [p + [i, total_evaluations_num, start_time] for i, p in enumerate(pairs_N_n)]
@@ -70,15 +80,15 @@ def evaluate_paper():
     :return:
     """
 
-    results = np.full((max_no_neighbours + 1, max_no_neighbours + 1), 0)
+    results = np.full((MAX_NO_NEIGHBOURS + 1, MAX_NO_NEIGHBOURS + 1), RESULTS_INITIAL_FILL_VAL)
     results = results.astype('float64')
-    with open(result_file, 'wb') as f:
+    with open(RESULT_FILE, 'wb') as f:
         np.save(f, results)
 
     with mp.Pool(processes=mp.cpu_count()) as pool:
-        out = pool.starmap(_sim_with_agents, generate_simulations_args(), chunksize=1)
+        out = pool.starmap(_sim_with_agents, generate_simulations_args(difficult_range_only=DIFFICULT_RANGE_ONLY), chunksize=1)
 
-    with open(result_file, 'rb') as f:
+    with open(RESULT_FILE, 'rb') as f:
         results = np.load(f)
 
     return results
@@ -152,4 +162,4 @@ if __name__ == '__main__':
     print("Start evaluation")
     results = evaluate_paper()
     print(results)
-    plot_results(results[1:, 1:].T, result_fig_file)
+    plot_results(results[1:, 1:].T, RESULT_FIG_FILE)
