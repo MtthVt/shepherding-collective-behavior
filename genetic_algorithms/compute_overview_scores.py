33f26186a9ac75b05b81658cf1c1f2a944e5e6de
adamnapieralski <adam.napieralski@gmail.com>
2022-01-06 18:48:49 +0000

    feat(ga-evaluation): use max range

--- genetic_algorithms/compute_overview_scores.py
+++ genetic_algorithms/compute_overview_scores.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-original_strombom_res_path = ''
-ga_strombom_res_path = ''
-ga_sigmoid_res_path = ''
-
 results_meta = [
     {
         'name': 'Original Strombom',
@@ -23,30 +19,32 @@ results_meta = [
     }
 ]
 
+analysis_range = 101
+
 base = np.zeros((140, 140), dtype=bool)
 mask_all = np.copy(base)
-mask_triangle = np.copy(base)
 
-for N in range(1, mask_all.shape[0]):
+for N in range(1, analysis_range):
     for n in range(1, N):
         mask_all[N,n] = True
 
-for N in range(1, mask_triangle.shape[0]):
-    for n in range(int(np.floor(3 * np.log2(N))), int(np.ceil(0.53 * N))):
-        mask_triangle[N,n] = True
+data_sigmoid = np.load('results/evaluation.sigmoid.transition_only.npy')
+mask_transit = data_sigmoid != -1
+mask_transit[:, analysis_range:] = False
+mask_transit[analysis_range:,:] = False
 
 
 for res in results_meta:
     data = np.load(res['res_path'])
 
-    data_masked = data[mask_triangle]
-    mean_triangle, std_triangle = np.mean(data_masked), np.std(data_masked)
+    data_masked = data[mask_transit]
+    mean_triangle, std_triangle = round(np.mean(data_masked), 3), round(np.std(data_masked), 3)
 
-    print(f"{res['name']} - triangle:\n\tmean:\t{mean_triangle}\tstd:\t{std_triangle}")
+    print(f"{res['name']} - transit:\t\tmean:\t{mean_triangle}\tstd:\t{std_triangle}")
 
     if res['name'] == 'GA Sigmoid':
         continue
 
     data_masked = data[mask_all]
-    mean_all, std_all = np.mean(data_masked), np.std(data_masked)
-    print(f"{res['name']} - all:\n\tmean:\t{mean_all}\tstd:\t{std_all}")
+    mean_all, std_all = round(np.mean(data_masked), 3), round(np.std(data_masked), 3)
+    print(f"{res['name']} - all:\t\tmean:\t{mean_all}\tstd:\t{std_all}")
